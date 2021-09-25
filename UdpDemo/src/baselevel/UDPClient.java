a883eb9dc0394fadec110725f5dba70a9a63f1ce
qzeng <qzeng@telenav.cn>
2021-09-25 17:24:03 +0800

    新增Udp基础学习

---
+++ UdpDemo/src/baselevel/UDPClient.java
@@ -0,0 +1,42 @@
+package baselevel;
+
+import java.io.IOException;
+import java.net.*;
+
+public class UDPClient {
+    public static void main(String[] args) throws SocketException {
+        DatagramSocket clientSocket = new DatagramSocket();
+        String hi = "hi";
+        byte[] bytes = new byte[4048];
+        byte[] bytes1 = hi.getBytes();
+        System.arraycopy(bytes1,0, bytes,0, bytes1.length);
+        InetAddress localHost = null;
+        try {
+            localHost = Inet4Address.getLocalHost();
+            DatagramPacket packet = new DatagramPacket(bytes,  //创建分组，并设置目的地址
+                    0,
+                    bytes.length,
+                    localHost,
+                    12000);
+            clientSocket.send(packet);
+            System.out.println("发送报文分组至" + packet.getAddress().getHostAddress() + ":" + packet.getPort());
+            System.out.println("*******************************************************");
+
+            while(true) {
+                clientSocket.receive(packet);
+                byte[] data = packet.getData();
+
+                System.out.println("接收到来自：" + packet.getAddress().getHostAddress() + ":"+ packet.getPort() + "的数据报文分组"+
+                        "; 数据长度 = " + data.length +
+                        "; 数据内容 = " + new String(data,"utf-8"));
+            }
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            clientSocket.close();
+        }
+    }
+}
---
+++ UdpDemo/src/baselevel/UDPServer.java
@@ -0,0 +1,35 @@
+package baselevel;
+
+import java.io.IOException;
+import java.net.*;
+
+public class UDPServer {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket serverSocket = new DatagramSocket(
+                    12000,
+                    InetAddress.getLocalHost());  //把套接字绑定到本地ip：端口12000上
+            System.out.println("服务端已经启动");
+            while (true) { // 轮询等待客户端的请求
+                byte[] bytes = new byte[4048];
+                DatagramPacket datagramPacket = new DatagramPacket(bytes, 0, bytes.length);//创建分组
+                serverSocket.receive(datagramPacket);//阻塞等待客户端的请求
+                System.out.println("收到客户端(" + datagramPacket.getAddress().getHostAddress() + ":" + datagramPacket.getPort() + ")发来的请求..."
+                        + new String(bytes, 0, bytes.length));
+                byte[] data = datagramPacket.getData();
+                String decodedData = new String(data);
+                int i = 0;
+                while (i < 10000) {
+                    datagramPacket.setData(("xi" + (i++)).getBytes());
+                    serverSocket.send(datagramPacket);// 发送响应报文给客户端
+                }
+
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
